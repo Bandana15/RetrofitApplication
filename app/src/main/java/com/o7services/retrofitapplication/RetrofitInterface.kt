8e9237e584b3f07c7613ff4eae285cdfa7561a9a
Bandana15 <bandana2408@gmail.com>
2022-04-20 19:06:59 +0530

    Initial commit

---
+++ app/src/main/java/com/o7services/retrofitapplication/RetrofitInterface.kt
@@ -0,0 +1,18 @@
+package com.o7services.retrofitapplication
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface RetrofitInterface {
+
+    @GET("users")
+    fun getUsers(): Call<UserResponse>
+
+    @GET("users/{userId}")
+    fun getSingleUser(@Path("userId") userId: Int): Call<UserResponseItem>
+
+    @GET("comments")
+    fun getcomments():Call<CommentsResponse>
+
+}
